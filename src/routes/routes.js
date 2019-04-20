98e362ab5013e6cd3262d43edf38a773e68dfdbb
Miangaly <miangaly.randriamifidy@gmail.com>
2019-04-20 22:31:33 +0400

    update routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,8 +6,8 @@ import Contact from '../components/contact/Contact.vue'
 import AdminRestaurant from "../components/back-office/restaurants/AdminRestaurant.vue";
 import RestaurantForm from "../components/back-office/restaurants/form-restaurants/RestaurantForm.vue";
 import Restaurants from '../components/restaurants/Restaurant.vue'
-import Login from '../components/login-subscribe/login/login.vue'
-import Subscribe from '../components/login-subscribe/subscribe/subscribe.vue'
+import Login from '../components/login-subscribe/login/Login.vue'
+import Subscribe from '../components/login-subscribe/subscribe/Subscribe.vue'
 
 const routes = [
     {
